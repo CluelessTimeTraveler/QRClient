067f420f10d09e3e25d8ca4bae0eb3d273ad8a6b
mario <macastro2@wpi.edu>
2019-10-29 14:36:30 -0400

    Created

---
+++ cmake-build-debug/CMakeFiles/QRClient.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/QRClient.dir/main.cpp.o"
+  "QRClient"
+  "QRClient.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/QRClient.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
