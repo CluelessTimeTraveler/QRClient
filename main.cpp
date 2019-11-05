c9d9b7fb552e950bb0637b2047b10106258994f2
mario <macastro2@wpi.edu>
2019-11-04 20:50:10 -0500

    Working Client, sends QR code, recieves URL, command line args mostly working

--- main.cpp
+++ main.cpp
@@ -6,16 +6,79 @@
 #include <string.h>
 #include <sys/stat.h>
 #include <fstream>
+#import <thread>
+#include <getopt.h>
+
+using namespace std;
 
 #define PORT 2012
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#define no_argument 0
+#define required_argument 1
+#define optional_argument 2
+
+
+int main(int argc, char *argv[]) {
+    std::cout << "-----QR Code Client-----" << std::endl;
     int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
+
     FILE* QRcode;
     struct stat st;
+
+    int opt;
+
+    int port = 2012;
+    std::string address = "127.0.0.1";
+    std::string filePath = "google.png";
+
+
+    const struct option longopts[] = {
+            {"PORT", optional_argument, NULL, 'P'},
+            {"ADDRESS", optional_argument, NULL, 'A'},
+            {"FILE", required_argument, NULL, 'F'},
+            {0, 0, 0, 0}
+    };
+
+
+
+
+    while(((opt = getopt_long(argc, argv, "PAF:", longopts, NULL)) != -1)){
+
+        //std::cout << "Arguement is optarg: " << optarg << std::endl;
+
+
+        switch(opt){
+            case 'P':
+                port = atoi(optarg);
+                break;
+            case 'A':
+                address = optarg;
+                break;
+            case 'F':
+                filePath = optarg;
+                break;
+            case '?':
+                std::cout << "\nUnknown arguments";
+                return 1;
+            case ':':
+                std::cout << "\nError: Need more arguments";
+                break;
+
+        }
+    }
+
+    if(opt == -1 && argc == 1){
+        std::cout << "Error: Need more arguments";
+        return 1;
+    }
+
+
+
+    std::cout << "\nStarting connection with..";
+    std::cout << "\nPort: " << port;
+    std::cout << "\nAddress: " << address;
+    std::cout << "\nQR Code File: " << filePath;
 
 
     char buffer[1024] = {0};
@@ -26,12 +89,12 @@ int main() {
     }
 
     serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    serv_addr.sin_port = htons(port);
 
 
 
 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET, address.c_str(), &serv_addr.sin_addr)<=0)
     {
         printf("\nInvalid address/ Address not supported \n");
         return -1;
@@ -43,39 +106,37 @@ int main() {
         printf("\nConnection Failed \n");
         return -1;
     }
-//    send(sock , hello , strlen(hello) , 0 );
-//    printf("Hello message sent\n");
-//    valread = read( sock , buffer, 1024);
-//    printf("%s\n",buffer );
 
-    QRcode = fopen("google.png","r");
-    stat("google.png",&st);
+    QRcode = fopen(filePath.c_str(),"r");
+    stat(filePath.c_str(),&st);
 
     int byteSize = st.st_size;
 
     send(sock, &byteSize, sizeof(int), 0);
-    std::cout << "Sent Size, was " <<  byteSize << std::endl;
+    std::cout << "\nSent Size, was " <<  byteSize << std::endl;
 
-//    int gamerNumber = 69420;
-//    send(sock, &gamerNumber, sizeof(int),0);
+
 
 
     char * imgBuffer = (char *)malloc(st.st_size);
-    //std::cout << "size of buffer: " << sizeof(imgBuffer);
-    //printf("File Size: %d bytes\n",st.st_size);
+
     fread(imgBuffer,st.st_size,1, QRcode);
 
-//    FILE * recievedFile;
-//    recievedFile = fopen("googleRec.png","w");
-//    fwrite(imgBuffer, st.st_size,1,recievedFile);
 
-//    std::ofstream image;
-//    image.open("googleRec.png");
-//    image << imgBuffer;
-//    image.close();
 
     send(sock, imgBuffer,st.st_size,0);
     std::cout << "Sent image";
+
+    int URLsize;
+
+
+    read(sock, &URLsize,sizeof(int));
+    std::cout << "\nReceived URL size of: " << URLsize;
+
+    char * recURL = static_cast<char *>(malloc(URLsize));
+
+    read(sock, recURL, URLsize);
+    std::cout << "\nRecieved URL: " << recURL;
 
     return 0;
 }
